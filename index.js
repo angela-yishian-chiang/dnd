5e94f27065626163582f8af3834d8695d3eaf256
angela-yishian-chiang <yishianchiang@gmail.com>
2025-04-02 06:11:33 +0000

    updated

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ app.use((req, res, next) => {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
