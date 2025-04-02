f47c3406b7d8f2781b1bb05b61d80f5879313219
angela-yishian-chiang <yishianchiang@gmail.com>
2025-04-02 05:59:07 +0000

    added logging to backend entry

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ import OpenAI from "openai";
 import path from "path";
 import { fileURLToPath } from "url";
 
+app.use((req, res, next) => {
+  console.log(`➡️ ${req.method} ${req.url}`);
+  next();
+});
+
 // Load environment variables
 dotenv.config();
 
